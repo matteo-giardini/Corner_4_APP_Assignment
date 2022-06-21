de87c932ef3204d6613e5fa733a91d4a9414d95e
Mona Lisa <matteogiardini@alumni.esade.edu>
2022-06-21 17:51:24 +0200

    docstrings_todo_list

---
+++ Connect_4_Draft.py
@@ -0,0 +1,38 @@
+''' TO DO LIST:
+Board Class:
+- initialize board with row and column
+- setup_board - ask user how many rows and columns to play with
+- make_board (if we do graphics, if we dont, let's draw it on the terminal)
+- update_board (maybe necessary (??), takes as input old board, new move and returns new board)
+
+- make_move
+- valid_location + place to first available row
+- check_horizontal
+- check_vertical
+- check_pos_diag
+- check_neg_diag
+- is_game_over - check if check functions above return True
+
+
+User Class:
+- initialize user (name, last_name, age, color (if we do graphics))
+- get_name
+- ask_input - returns column number
+- ...
+
+
+def main():
+    count_turns = 0
+    while is_game_over == False:
+        ### ask for user input
+        count_turns +=1
+
+
+    while is_game_over == False:
+        ### Game dynamics 
+        count_turns +=1
+
+
+if __name__ == __main__:
+    main()
+'''
